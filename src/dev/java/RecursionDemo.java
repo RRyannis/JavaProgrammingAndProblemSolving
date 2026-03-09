10cbcefd59f0b21d75d226958ab555a086422a87
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-03-09 09:32:35 +0200

    recursion demo

--- src/dev/java/RecursionDemo.java
+++ src/dev/java/RecursionDemo.java
@@ -2,11 +2,22 @@ package dev.java;
 
 public class RecursionDemo {
     public static void main(String[] args) {
-        System.out.println(factorial(3));
+        //System.out.println(factorial(3));
+        //System.out.println(sum(4));
+        System.out.println(fibonacci(6));
     }
 
     private static int factorial(int n) {
         if (n == 0) return 1;
         return n*factorial(n-1);
+    }
+    private static int sum(int i){
+        if (i == 0) return 0;
+        return i + sum(i-1);
+    }
+    private static int fibonacci(int n){
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fibonacci(n - 1) + fibonacci(n -2);
     }
 }
