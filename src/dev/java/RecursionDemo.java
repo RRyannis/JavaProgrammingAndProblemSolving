8ff616af25a67d0c5e03d3273e4c840e6d384f60
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-03-10 22:37:08 +0200

    minor

--- src/dev/java/RecursionDemo.java
+++ src/dev/java/RecursionDemo.java
@@ -4,7 +4,9 @@ public class RecursionDemo {
     public static void main(String[] args) {
         //System.out.println(factorial(3));
         //System.out.println(sum(4));
-        System.out.println(fibonacci(6));
+        //System.out.println(fibonacci(6));
+        methodA(3);
+        methodA(5);
     }
 
     private static int factorial(int n) {
@@ -19,5 +21,15 @@ public class RecursionDemo {
         if (n == 0) return 0;
         if (n == 1) return 1;
         return fibonacci(n - 1) + fibonacci(n -2);
+    }
+    public static void methodA(int n)
+    {
+        if (n < 1)
+            System.out.println('B');
+        else
+        {
+            methodA(n - 1);
+            System.out.println('R');
+        }
     }
 }
