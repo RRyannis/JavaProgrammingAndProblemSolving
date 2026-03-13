1556d0367080512c02f11e602c33bd7d6ff45d44
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-03-13 11:29:47 +0200

    HashSet demo

--- src/dev/java/DynamicDataStructures.java
+++ src/dev/java/DynamicDataStructures.java
@@ -1,15 +1,33 @@
 package dev.java;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class DynamicDataStructures {
     public static void main(String[] args) {
-        String[] anArray = new String[20];
-        ArrayList<String> aList = new ArrayList<String>(3);
-        aList.add("One");
-        aList.add("Two");
-        aList.add("Three");
-        aList.add(0,"Four");
-        System.out.println(aList.get(0));
+//        String[] anArray = new String[20];
+//        ArrayList<String> aList = new ArrayList<String>(3);
+//        aList.add("One");
+//        aList.add("Two");
+//        aList.add("Three");
+//        aList.add(0,"Four");
+//        System.out.println(aList.get(0));
+        HashSet<Integer> intSet = new HashSet<Integer>();
+        intSet.add(1);
+        intSet.add(2);
+        intSet.add(3);
+        intSet.add(2);
+        printSet(intSet);
+        System.out.println("Set contains 2: " +
+                intSet.contains(2));
+        System.out.println("Set contains 4: " +
+                intSet.contains(4));
+    }
+    public static void printSet(HashSet<Integer> intSet){
+        System.out.println("The set contains: ");
+        for (Object obj: intSet.toArray()){
+            Integer num = (Integer) obj;
+            System.out.println(num.intValue());
+        }
     }
 }
