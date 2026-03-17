ddbabeea3b11ac8b81c4edd3249a9e69bc3c7478
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-03-17 20:57:30 +0200

    java collections

--- src/dev/java/DynamicDataStructures.java
+++ src/dev/java/DynamicDataStructures.java
@@ -1,6 +1,7 @@
 package dev.java;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class DynamicDataStructures {
@@ -12,19 +13,22 @@ public class DynamicDataStructures {
 //        aList.add("Three");
 //        aList.add(0,"Four");
 //        System.out.println(aList.get(0));
-        HashSet<Integer> intSet = new HashSet<Integer>();
-        intSet.add(1);
-        intSet.add(2);
-        intSet.add(3);
-        intSet.add(2);
-        intSet.add(5);
-        intSet.add(4);
-        intSet.add(7);
-        printSet(intSet);
-        System.out.println("Set contains 2: " +
-                intSet.contains(2));
-        System.out.println("Set contains 4: " +
-                intSet.contains(4));
+//        HashSet<Integer> intSet = new HashSet<Integer>();
+//        intSet.add(1);
+//        intSet.add(2);
+//        intSet.add(3);
+//        intSet.add(2);
+//        intSet.add(5);
+//        intSet.add(4);
+//        intSet.add(7);
+//        printSet(intSet);
+//        System.out.println("Set contains 2: " +
+//                intSet.contains(2));
+//        System.out.println("Set contains 4: " +
+//                intSet.contains(4));
+        HashMap<String, Integer> mountains = new HashMap<String, Integer>();
+        mountains.put("Everest", 29029);
+        System.out.println("Everest in the map: " + mountains.containsKey("Everest"));
     }
     public static void printSet(HashSet<Integer> intSet){
         System.out.println("The set contains: ");
