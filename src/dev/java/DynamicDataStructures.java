6730b3321b32500d86423fcee1b084ad70011b6c
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-03-23 11:17:30 +0200

    collections

--- src/dev/java/DynamicDataStructures.java
+++ src/dev/java/DynamicDataStructures.java
@@ -12,7 +12,7 @@ public class DynamicDataStructures {
 //        aList.add("Two");
 //        aList.add("Three");
 //        aList.add(0,"Four");
-        //aList.add(0,"Nine");
+//        aList.add(0,"Nine");
 //        System.out.println(aList.get(0));
 //        HashSet<Integer> intSet = new HashSet<Integer>();
 //        intSet.add(1);
@@ -27,27 +27,33 @@ public class DynamicDataStructures {
 //                intSet.contains(2));
 //        System.out.println("Set contains 4: " +
 //                intSet.contains(4));
-        HashMap<String, Integer> mountains = new HashMap<String, Integer>();
-        mountains.put("Everest", 29029);
-        System.out.println("Everest in the map: " + mountains.containsKey("Everest"));
-        mountains.put("K2", 28251);
-        mountains.put("Kangchenjunga", 28169);
-        mountains.put("Denali", 20335);
-        mountains.put("Fuji", 12390);
-        printMap(mountains);
-        System.out.println("Denali in the map: " +
-                mountains.containsKey("Denali"));
-        System.out.println();
-        System.out.println("Changing height of Denali.");
-        mountains.put("Denali", 20320);
-        printMap(mountains);
-        System.out.println("Removing Kangchenjunga.");
-        mountains.remove("Kangchenjunga");
-        printMap(mountains);
+//        HashMap<String, Integer> mountains = new HashMap<String, Integer>();
+//        mountains.put("Everest", 29029);
+//        System.out.println("Everest in the map: " + mountains.containsKey("Everest"));
+//        mountains.put("K2", 28251);
+//        mountains.put("Kangchenjunga", 28169);
+//        mountains.put("Denali", 20335);
+//        mountains.put("Fuji", 12390);
+//        printMap(mountains);
+//        System.out.println("Denali in the map: " +
+//                mountains.containsKey("Denali"));
+//        System.out.println();
+//        System.out.println("Changing height of Denali.");
+//        mountains.put("Denali", 20320);
+//        printMap(mountains);
+//        System.out.println("Removing Kangchenjunga.");
+//        mountains.remove("Kangchenjunga");
+//        printMap(mountains);
         HashSet<String> colors = new HashSet<String>();
         colors.add("red");
         colors.add("blue");
         printSet(colors);
+        System.out.println("Colors contains blue: " + colors.contains("blue"));
+        colors.remove("blue");
+        HashMap<Integer, String> studentIDS = new HashMap<Integer, String>();
+        studentIDS.put(5531, "George");
+        studentIDS.put(9102, "John");
+        System.out.println(studentIDS.get(9102));
     }
 //    public static void printSet(HashSet<Integer> intSet){
 //        System.out.println("The set contains: ");
